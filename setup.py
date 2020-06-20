0225f099f3770a4012cb52bce7c1cef021b20bf7
Peter Fison <peter@southwestlondon.tv>
2020-06-20 12:06:02 +0100

    ~ distutils.core

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import pathlib
-from setuptools import setup
-# from distutils.core import setup
+# from setuptools import setup
+from distutils.core import setup
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
