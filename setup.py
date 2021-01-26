26a1c49ca2cb5a7350d7f0b4910ee219ce818f0d
Peter Fison <peter@southwestlondon.tv>
2021-01-26 10:42:51 +0000

    Version 1.8.0rc5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 HERE = Path(__file__).parent
 NAME = "cleverdict"
 GITHUB_ID = "Pfython"
-VERSION = "1.8.0rc4"
+VERSION = "1.8.0rc5"
 DESCRIPTION = "A JSON-friendly data structure which allows both object attributes and dictionary keys and values to be used simultaneously and interchangeably."
 LICENSE = "MIT License"
 AUTHOR = "Peter Fison"
@@ -16,7 +16,7 @@ EMAIL = "peter@southwestlondon.tv"
 URL = "https://github.com/Pfython/cleverdict"
 KEYWORDS = "cleverdict, data, attribute, key, JSON, autosave, autodelete, to_json, from_json, to_lines, from_lines, to_list, from_list, value, attributes, keys, values, database, utility, tool, clever, dictionary, att, __getattr__, __setattr__, getattr, setattr"
 CLASSIFIERS = "Development Status :: 5 - Production/Stable, Intended Audience :: Developers, Operating System :: OS Independent, Programming Language :: Python :: 3.6, Programming Language :: Python :: 3.7, Programming Language :: Python :: 3.8, Programming Language :: Python :: 3.9, Topic :: Software Development :: Libraries :: Python Modules, Topic :: Software Development :: Object Brokering, Topic :: System :: Logging, Topic :: Utilities, License :: OSI Approved :: MIT License"
-REQUIREMENTS = ""
+REQUIREMENTS = "cleverdict, "
 
 
 def comma_split(text: str):
